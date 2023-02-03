38825d58f753748fdbd5a2da21a063ea03ee064e
benNjogu <kamandemachafi@gmail.com>
2023-02-03 06:14:52 +0300

    backend work

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,19 @@
-export const schemaTypes = []
+import testimonials from './testimonials'
+import abouts from './abouts'
+import brand from './brands'
+import contact from './contact'
+import experience from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes = [
+  testimonials,
+  abouts,
+  brand,
+  contact,
+  experience,
+  skills,
+  workExperience,
+  works,
+]
